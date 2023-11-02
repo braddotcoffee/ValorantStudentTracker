c650176d75063a93eb60c15effad3604e28311d3
Obliie <kiirohotaru@protonmail.com>
2023-11-02 23:24:02 +0000

    feat: update to support backend

--- src/app/coach.service.ts
+++ src/app/coach.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, firstValueFrom, timeout } from "rxjs";
+import { BehaviorSubject, Observable, firstValueFrom, map, timeout } from "rxjs";
 import { CONFIG_SERVICE_REQUEST_TIMEOUT, STORAGE_COACH_NAME_KEY, STORAGE_SPREADSHEET_ID_KEY, getBackendUrl } from "src/main";
 import { Coach } from "src/types/student";
 import { displaySnackBarError } from "./util/error-util";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
@@ -12,6 +13,7 @@ export class CoachService {
     activeCoach: BehaviorSubject<string>;
 
     constructor(
+        private httpClient: HttpClient,
         private snackBar: MatSnackBar
     ) {
       this.activeCoach = new BehaviorSubject(localStorage.getItem(STORAGE_COACH_NAME_KEY) ?? "");
@@ -30,11 +32,6 @@ export class CoachService {
     }
 
     async getCoachNames(): Promise<Observable<string[]>> {
-      // TODO Remove test data
-      return Promise.resolve(new Observable(obs => {
-        obs.next(["Woohoojin", "Dopai", "Obli", "Brad"]);
-      }));
-      /* TODO Uncomment when supported by backend
         return this.httpClient
           .get(
             this.buildGetCoachNamesUrl()
@@ -42,31 +39,16 @@ export class CoachService {
           .pipe(
             map((response: any) => response.coaches)
           )
-      */
     }
 
     async getCoach(coachName: string): Promise<Observable<Coach>> {
-      // TODO Remove test data
-      return Promise.resolve(new Observable(obs => {
-        if (coachName === "Woohoojin") {
-          return obs.next({
-            spreadsheetId: "1httLutCH3tj1G8aM5QjeklfvwPsOAsLGulWAQ4g8LGo"
-          });
-        } else if (coachName === "Dopai") {
-          return obs.next({
-            spreadsheetId: "13KR000AgHoG5VKtufdKxuTOV9Ixo8Aj1ZXovyPliuHE"
-          });
-        }
-      }));
-      /* TODO Uncomment when supported by backend
         return this.httpClient
           .get(
             this.buildGetCoachUrl(coachName)
           )
           .pipe(
-            map((response: any) => response.coach)
+            map((response: any) => response)
           )
-      */
     }
 
     async loadCoach(coachName: string): Promise<boolean> {
